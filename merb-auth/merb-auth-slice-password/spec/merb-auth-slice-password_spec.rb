e4c20398aaa89dafac2a5f217257e3f127416f16
Yehuda Katz <wycats@gmail.com>
2008-10-29 13:32:47 -0700

    Removes files

--- merb-auth/merb-auth-slice-password/spec/merb-auth-slice-password_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/spec_helper'
-
-describe "MerbAuthSlicePassword (module)" do
-  
-  it "should have proper specs"
-  
-end
--- merb-gen/lib/generators/templates/component/resource_controller/spec/requests/%file_name%_spec.rb
+++
@@ -1,1 +0,0 @@
-require File.join(File.dirname(__FILE__), <%= go_up(modules.size + 1) %>, 'spec_helper.rb')
